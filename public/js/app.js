f69c3d732253355825427496a5def871fe422642
gordon129315 <gordonyuzr@gmail.com>
2019-09-18 14:07:15 -0400

    add cities plugins

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,10 @@
 console.log('javaScript file')
 
 const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
+const search = document.querySelector('#city')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+
 
 
 weatherForm.addEventListener('submit', (e) => {
